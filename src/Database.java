998e867eeee74fab7e3a372803900c21e411f04c
MuisScream104 <liamzanfardini@gmail.com>
2025-11-04 20:29:51 +0100

    Commit commit commit cooommiiit

--- src/Database.java
+++ src/Database.java
@@ -1,4 +1,132 @@
-package PACKAGE_NAME;
+import java.sql.*;
 
 public class Database {
+
+    private static Database instance;
+    private Connection connection;
+
+    private Database() throws Exception {
+        String url = "jdbc:sqlite:database/sushi.db";
+        connection = DriverManager.getConnection(url);
+        System.out.println("Connected to database");
+    }
+
+    public static Database getInstance() throws Exception {
+        if (instance == null) {
+            instance = new Database();
+        }
+        return instance;
+    }
+
+    public String selectAll() {
+        String result = "";
+
+        try {
+            if (connection == null || !connection.isValid(5)) {
+                System.err.println("Connection is null or invalid");
+                return null;
+            }
+        } catch (SQLException e) {
+            System.err.println("Connection is null or invalid");
+            return null;
+        }
+
+        String query = "SELECT * FROM menu";
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                result += rs.getString("id") + "\t";
+                result += rs.getString("piatto") + "\t";
+                result += rs.getString("prezzo") + "\t";
+                result += rs.getString("quantita") + "\n";
+            }
+
+
+        } catch (SQLException e) {
+            System.err.println("Error in query: " + query);
+            return null;
+        }
+
+        return result;
+    }
+
+
+    public boolean insert(String piatto, double prezzo, int quantita) {
+        //Controlliamo che il database sia ancora collegato
+        try {
+            if (connection == null || !connection.isValid(5)) {
+                System.err.println("Connection is null or invalid");
+                return false;
+            }
+        } catch (SQLException e) {
+            System.err.println("Connection is null or invalid");
+            return false;
+        }
+
+
+
+        String query = "INSERT INTO menu(piatto, prezzo, quantita) VALUES ( "+piatto+", "+prezzo+", "+quantita+")";
+        try{
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.executeUpdate();
+        }
+        catch (SQLException e) {
+            System.err.println("Insert failed");
+            return false;
+        }
+
+        return true;
+    }
+    public boolean delete(String piatto) {
+        //Controlliamo che il database sia ancora collegato
+        try {
+            if (connection == null || !connection.isValid(5)) {
+                System.err.println("Connection is null or invalid");
+                return false;
+            }
+        } catch (SQLException e) {
+            System.err.println("Connection is null or invalid");
+            return false;
+        }
+
+
+
+        String query = "DELETE FROM menu WHERE piatto = '"+piatto+"'";
+        try{
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.executeUpdate();
+        }
+        catch (SQLException e) {
+            System.err.println("Delete failed");
+            return false;
+        }
+
+        return true;
+    }
+    public boolean Update(String piatto, double prezzo, int quantita) {
+        try {
+            if (connection == null || !connection.isValid(5)) {
+                System.err.println("Connection is null or invalid");
+                return false;
+            }
+        } catch (SQLException e) {
+            System.err.println("Connection is null or invalid");
+            return false;
+        }
+
+
+
+        String query = "UPDATE menu SET prezzo='"+prezzo+"', quantita='"+quantita+"'";
+        try{
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.executeUpdate();
+        }
+        catch (SQLException e) {
+            System.err.println("Update failed");
+            return false;
+        }
+
+        return true;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Database database = null;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        if(database.insert("Udon",10,1))
+            System.out.printf("Piatto inserito con successo");
+
+        if(database.Update("Udon",11,1))
+            System.out.printf("Piatto aggiornato con successo");
+
+        if(database.delete("Udon"))
+            System.out.printf("Piatto cancellato con successo");
+
+        System.out.printf(database.selectAll());
     }
 }
